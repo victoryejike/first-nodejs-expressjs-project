b49be093e58c45d25006901f8c86463023f6b8a2
victory Ejike <victoryejike@gmail.com>
2020-07-16 21:40:51 +0100

    redirecting new leads to the leads route

--- controllers/landing.js
+++ controllers/landing.js
@@ -8,11 +8,24 @@ exports.submit_lead = function (req, res, next) {
   console.log("lead email:", req.body.lead_email);
   return models.Lead.create({
     email: req.body.lead_email,
-  }).then((lead) => {
-    res.redirect("/");
-  });
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.sendStatus(500);
-  // });
+  })
+    .then((lead) => {
+      res.redirect("/leads");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+};
+
+exports.show_leads = function (req, res, next) {
+  return models.Lead.findAll()
+    .then((leads) => {
+      // console.log(leads);
+      res.render("landing", { title: "Express", leads: leads });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 };
